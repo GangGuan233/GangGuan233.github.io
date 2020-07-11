eaed69bcbd0397c7f62664e5d6dbd3d21f084f57
GangGuan <51810407+GangGuan233@users.noreply.github.com>
2020-07-12 01:23:13 +0800

    Update stylt.js

--- style/stylt.js
+++ style/stylt.js
@@ -1,15 +1,15 @@
-var obj;
+varobj;
 var href;
 function btn() {
-
-    obj = document.getElementById("baidu");
-    var tit = obj.attributes["href"].nodeValue;
-    obj = null;
-    obj = document.getElementById("iptText").value;
-    var question = obj;
-    obj = null;
-    href = tit + "s?wd=" + question;
-    window.location = href;
-    console.log("href=" + href);
-
+    var question = document.getElementById("iptText").value;
+    if (question != "") {
+        obj = document.getElementById("baidu");
+        var tit = obj.attributes["href"].nodeValue;
+        obj = null;
+        href = tit + "s?wd=" + question;
+        window.location = href;
+        console.log("href=" + href);
+    } else {
+        location.reload()
+    }
 };
